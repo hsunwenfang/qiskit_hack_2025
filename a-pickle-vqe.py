9d8598dc7b43e33a5fc9eb7cb4e3ed01e4a41f33
Yeonsoo Kim <yeonsookim.dev@gmail.com>
2025-08-14 11:17:28 +0900

    seperate vqe info save and sqd info load to two files

--- a-pickle-vqe.py
+++ a-pickle-vqe.py
@@ -86,6 +86,11 @@ def vqe_callback(counts, parameters, value, metadata):
     parameters_vars.append(parameters)
     estimated_value.append(value)
     meta_dict.append(metadata)
+
+    # Save VQE info
+    vqe_info = (ansatz, evaluation_count, parameters_vars, estimated_value, meta_dict)
+    Path("vqe_info.pickle").write_bytes(pickle.dumps(vqe_info))
+
     print(f"iter: {counts:4d}, energy: {value:.5f}, parameters: {parameters}")
 
 
@@ -96,69 +101,65 @@ calc = GroundStateEigensolver(mapper, solver)
 res = calc.solve(es_problem)
 print('total_energy: ', res.total_energies[0])
 
-# Save VQE info
-vqe_info = (ansatz, evaluation_count, parameters_vars, estimated_value, meta_dict)
-Path("vqe_info.pickle").write_bytes(pickle.dumps(vqe_info))
-
 # Load VQE info
-vqe_info = pickle.loads(Path("vqe_info.pickle").read_bytes())
-(ansatz, evaluation_count, parameters_vars, estimated_value, meta_dict) = vqe_info
+# vqe_info = pickle.loads(Path("vqe_info.pickle").read_bytes())
+# (ansatz, evaluation_count, parameters_vars, estimated_value, meta_dict) = vqe_info
 
-ansatz.measure_all()
+# ansatz.measure_all()
 
-pass_manager = generate_preset_pass_manager(
-    optimization_level=3, backend=backend, #initial_layout=initial_layout
-)
-pass_manager.pre_init = ffsim.qiskit.PRE_INIT
-isa_circuit = pass_manager.run(ansatz)
-job = sampler.run([(isa_circuit, parameters_vars[-1])], shots=10_000)
-primitive_result = job.result()
-print('primitive result:', primitive_result)
-pub_result = primitive_result[0]
-print(pub_result)
-bit_array = pub_result.data.meas
-counts = pub_result.data.meas.get_counts()
+# pass_manager = generate_preset_pass_manager(
+#     optimization_level=3, backend=backend, #initial_layout=initial_layout
+# )
+# pass_manager.pre_init = ffsim.qiskit.PRE_INIT
+# isa_circuit = pass_manager.run(ansatz)
+# job = sampler.run([(isa_circuit, parameters_vars[-1])], shots=10_000)
+# primitive_result = job.result()
+# print('primitive result:', primitive_result)
+# pub_result = primitive_result[0]
+# print(pub_result)
+# bit_array = pub_result.data.meas
+# counts = pub_result.data.meas.get_counts()
 
-def sqd_callback(results: list[SCIResult]):
-    result_history.append(results)
-    iteration = len(result_history)
-    print(f"Iteration {iteration}")
-    for i, result in enumerate(results):
-        print(f"\tSubsample {i}")
-        print(f"\t\tEnergy: {result.energy + nuclear_repulsion_energy}")
-        print(f"\t\tSubspace dimension: {np.prod(result.sci_state.amplitudes.shape)}")
+# def sqd_callback(results: list[SCIResult]):
+#     result_history.append(results)
+#     iteration = len(result_history)
+#     print(f"Iteration {iteration}")
+#     for i, result in enumerate(results):
+#         print(f"\tSubsample {i}")
+#         print(f"\t\tEnergy: {result.energy + nuclear_repulsion_energy}")
+#         print(f"\t\tSubspace dimension: {np.prod(result.sci_state.amplitudes.shape)}")
 
-# SQD options
-energy_tol = 1e-3
-occupancies_tol = 1e-3
-max_iterations = 5
+# # SQD options
+# energy_tol = 1e-3
+# occupancies_tol = 1e-3
+# max_iterations = 5
 
-# Eigenstate solver options
-num_batches = 1
-samples_per_batch = 300
-symmetrize_spin = True
-carryover_threshold = 1e-4
-max_cycle = 100
+# # Eigenstate solver options
+# num_batches = 1
+# samples_per_batch = 300
+# symmetrize_spin = True
+# carryover_threshold = 1e-4
+# max_cycle = 100
 
-sci_solver = partial(solve_sci_batch, spin_sq=0.0, max_cycle=max_cycle)
-result_history = []
+# sci_solver = partial(solve_sci_batch, spin_sq=0.0, max_cycle=max_cycle)
+# result_history = []
 
-result = diagonalize_fermionic_hamiltonian(
-    hcore,
-    eri,
-    bit_array,
-    samples_per_batch=samples_per_batch,
-    norb=num_orbitals,
-    nelec=nelec,
-    num_batches=num_batches,
-    energy_tol=energy_tol,
-    occupancies_tol=occupancies_tol,
-    max_iterations=max_iterations,
-    sci_solver=sci_solver,
-    symmetrize_spin=symmetrize_spin,
-    carryover_threshold=carryover_threshold,
-    callback=sqd_callback,
-)
+# result = diagonalize_fermionic_hamiltonian(
+#     hcore,
+#     eri,
+#     bit_array,
+#     samples_per_batch=samples_per_batch,
+#     norb=num_orbitals,
+#     nelec=nelec,
+#     num_batches=num_batches,
+#     energy_tol=energy_tol,
+#     occupancies_tol=occupancies_tol,
+#     max_iterations=max_iterations,
+#     sci_solver=sci_solver,
+#     symmetrize_spin=symmetrize_spin,
+#     carryover_threshold=carryover_threshold,
+#     callback=sqd_callback,
+# )
 
 
 
